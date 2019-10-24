cfca1cc2f4ac61b376e0cd5927a993b597cf30df
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-10-24 12:37:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === whisper(string)) return "I can\'t hear you!"
   else if (string === "I love you, Grandma.") return "I love you, too."
-  else if (string === shout(string)) return 
+  else if (string === shout(string)) return "YES INDEED!"
 }
 
